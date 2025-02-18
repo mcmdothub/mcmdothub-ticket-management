ef180063d71fed395f485714d7557d011fa9005f
cristian <cristian.marin@linearsystem.it>
2025-02-18 16:14:27 +0100

    Creating the Domain Project

---
+++ TicketManagement.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace TicketManagement.Domain.Common
+{
+    // Base class "AuditableEntity"
+    public class AuditableEntity
+    {
+        // will contain a number of base properties
+        // I want every other entity to have full as a logging support for tracking purposes in my data store
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
